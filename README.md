318c98598abd2e1d1d65016180d85c9e7df8070b
TulioDesenv <tulio.dev@outlook.com>
2024-03-29 10:24:00 -0400

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews-local
+
 Implementação do https://www.tabnews.com.br para o curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Marcos Tulio dos Santos</h1>
+function Home() {
+  return <h1>Marcos Tulio dos Santos</h1>;
 }
 
 export default Home;
